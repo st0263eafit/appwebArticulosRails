fe9fb9dead76dc221fceb51d2678f564f3c8a013
Edwin Montoya Munera <emontoya@eafit.edu.co>
2017-07-28 22:42:29 -0500

    Saving data in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,17 @@
 class ArticlesController < ApplicationController
+
   def new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
     @article.save
     redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 
 end
